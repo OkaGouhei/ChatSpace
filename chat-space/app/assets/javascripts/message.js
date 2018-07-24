1e4a3a0f58750542cc3a46e4935b63ee80a14364
OkaGouhei <okagouhei@okagoutairanoMacBook-puro.local>
2018-07-24 14:14:24 +0900

    create_HTML
    6. 返ってきたJSONをdoneメソッドで受取り、HTMLを作成する.

--- chat-space/app/assets/javascripts/message.js
+++ chat-space/app/assets/javascripts/message.js
@@ -1,4 +1,23 @@
 $(function(){
+  function buildHTML(message){
+  var html = `<div class="main__body__messages__message">
+                <div class="main__body__messages__message__username">
+                  ${ message.user_name }
+                </div>
+                <div class="main__body__messages__message__date">
+                  ${ message.created_at}
+                </div>
+                <div class="main__body__messages__message__content">
+                  <p class="lower__message__content">
+                  ${ message.content }
+                  </p>
+                  <p class="lower__message__image">
+                  ${ message.image }
+                  </p>
+                </div>
+              </div>`
+  return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,4 +30,15 @@ $(function(){
       processData: false,
       contentType: false
   })
+      .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.main__footer__form__input').prop('disabled', false);
+      scroll()
+      $('#new_message')[0].reset();
+    })
+      .fail(function(){
+      alert('error');
+    })
+})
 })
